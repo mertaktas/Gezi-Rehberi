cd3863c69246e8d18339a799b1354bd2f29642c6
mert <mertaktas231@gmail.com>
2019-10-22 21:53:03 +0300

    login-register sayfaları yapıldı. Kayıt oluşturma ve giriş yapma ayarları yapıldı.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
     Travel = require("./models/travel"),
     Comment = require("./models/comment"),
     User = require("./models/user"),
@@ -19,6 +21,17 @@ app.use(bodyParser.urlencoded({
 app.set("view engine", "ejs");
 seedDB();
 
+// PASSPORT CONFIG
+app.use(require("express-session")({
+    secret: "pasword pasword pasword",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/", function (req, res) {
@@ -31,7 +44,7 @@ app.get("/travels", function (req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render("index", {
+            res.render("travels/index", {
                 travels: allTravels
             });
         }
@@ -42,9 +55,11 @@ app.get("/travels", function (req, res) {
 app.post("/travels", function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
+    var desc = req.body.description;
     var newTravel = {
         name: name,
-        image: image
+        image: image,
+        description: desc
     }
     Travel.create(newTravel, function (err, newlyCreated) {
         if (err) {
@@ -58,7 +73,7 @@ app.post("/travels", function (req, res) {
 
 // CREATE
 app.get("/travels/new", function (req, res) {
-    res.render("new");
+    res.render("travels/new");
 });
 
 // SHOW 
@@ -67,13 +82,98 @@ app.get("/travels/:id", function (req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render("show", {
+            res.render("travels/show", {
                 travel: foundTravel
             });
         }
     })
 });
 
+//COMMENTS ROUTES
+
+
+app.get("/travels/:id/comments/new", isLoggedIn, function (req, res) {
+    // find travel by id
+    Travel.findById(req.params.id, function (err, travel) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", {
+                travel: travel
+            });
+        }
+    })
+});
+
+app.post("/travels/:id/comments", isLoggedIn, function (req, res) {
+    //lookup campground using ID
+    Travel.findById(req.params.id, function (err, travel) {
+        if (err) {
+            console.log(err);
+            res.redirect("/travels");
+        } else {
+            Comment.create(req.body.comment, function (err, comment) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    travel.comments.push(comment);
+                    travel.save();
+                    res.redirect('/travels/' + travel._id);
+                }
+            });
+        }
+    });
+    //create new comment
+    //connect new comment to campground
+    //redirect campground show page
+});
+
+//  ===========
+// AUTH ROUTES
+//  ===========
+
+// show register form
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+//handle sign up logic
+app.post("/register", function (req, res) {
+    var newUser = new User({
+        username: req.body.username
+    });
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/travels");
+        });
+    });
+});
+
+// show login form
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+// handling login logic
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/travels",
+    failureRedirect: "/login"
+}), function (req, res) {});
+
+// logic route
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/travels");
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen("3000", function () {
     console.log("Server Çalıştı...");
