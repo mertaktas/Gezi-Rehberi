38fe53f961c1d312b8d979875916e8e615d2b1b2
mert <mertaktas231@gmail.com>
2019-10-25 02:30:51 +0300

    landing page sayfası düzenlendi. background-slider yapıldı.

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,7 @@ router.post("/register", function (req, res) {
             return res.render("register");
         }
         passport.authenticate("local")(req, res, function () {
-            req.flash("success", "gezinFO'\ya hoşgeldin " + user.username);
+            req.flash("success", "GezinFO'ya hoşgeldin " + user.username);
             res.redirect("/travels");
         });
     });
