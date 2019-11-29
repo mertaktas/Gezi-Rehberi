c429ea62434c7639c38a4fccd0f8633b6e89db2e
mert <mertaktas231@gmail.com>
2019-11-29 23:23:01 +0300

    Düzenlemeler yapıldı.

--- app.js
+++ app.js
@@ -10,13 +10,10 @@ var express = require("express"),
     Comment = require("./models/comment"),
     User = require("./models/user")
 
-
-// Routes
+// Routes File
 var commentRoutes = require("./routes/comments"),
     travelRoutes = require("./routes/travels"),
     indexRoutes = require("./routes/index")
-
-
 
 // Local Database
 // mongoose.connect("mongodb://localhost:27017/gezinfo", {
@@ -30,7 +27,6 @@ mongoose.connect("mongodb+srv://mert:mert@cluster0-ztbim.mongodb.net/test?retryW
     useUnifiedTopology: true
 });
 
-
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -39,9 +35,6 @@ mongoose.set('useFindAndModify', false);
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
-// seedDB();
-
-// PASSPORT CONFIG
 app.use(require("express-session")({
     secret: "pasword pasword pasword",
     resave: false,
@@ -59,7 +52,6 @@ app.use(function (req, res, next) {
     res.locals.success = req.flash("success");
     next();
 });
-
 app.use("/", indexRoutes);
 app.use("/travels", travelRoutes);
 app.use("/travels/:id/comments", commentRoutes);
--- middleware/index.js
+++ middleware/index.js
@@ -1,12 +1,12 @@
 var Travel = require("../models/travel");
 var Comment = require("../models/comment");
 
-// all the middleware
-
+// All the Middleware
 var middlewareObj = {
 
 };
 
+// Check Edit Travel
 middlewareObj.checkTravelOwnership = function (req, res, next) {
     if (req.isAuthenticated()) {
         Travel.findById(req.params.id, function (err, foundTravel) {
@@ -17,17 +17,21 @@ middlewareObj.checkTravelOwnership = function (req, res, next) {
                 if (foundTravel.author.id.equals(req.user._id)) {
                     next();
                 } else {
+
                     req.flash("error", "Bunu yapma izniniz yok.");
                     res.redirect("back");
+
                 }
             }
         });
     } else {
         req.flash("error", "Giriş yapmanız gerek.");
         res.redirect("back");
+
     }
 }
 
+// Check Edit Comment
 middlewareObj.checkCommentOwnership = function (req, res, next) {
     if (req.isAuthenticated()) {
         Comment.findById(req.params.comment_id, function (err, foundComment) {
@@ -38,14 +42,17 @@ middlewareObj.checkCommentOwnership = function (req, res, next) {
                 if (foundComment.author.id.equals(req.user._id)) {
                     next();
                 } else {
+
                     req.flash("error", "Bunu yapma izniniz yok.");
                     res.redirect("back");
+
                 }
             }
         });
     } else {
         req.flash("error", "Giriş yapmanız gerek.");
         res.redirect("back");
+
     }
 }
 
@@ -53,8 +60,10 @@ middlewareObj.isLoggedIn = function (req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
+
     req.flash("error", "Giriş yapmanız gerek.");
     res.redirect("/login");
+
 }
 
 module.exports = middlewareObj;
